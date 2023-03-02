f6f61ca152e974988f4a05b655369070e24a7d9b
uditnm <udit20001@gmail.com>
2023-03-02 15:54:25 +0530

    [Udit] added model class for the contacts.

--- AddressBook_System/Contacts.cs
+++ AddressBook_System/Contacts.cs
@@ -8,5 +8,19 @@ namespace AddressBook_System
 {
     internal class Contacts
     {
+        string firstname, lastname, email, address, city, state;
+        int phoneno, zip;
+
+        public Contacts(string firstname, string lastname, string address, string city, string state, int zip, int phoneno, string email)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+            this.phoneno = phoneno;
+            this.email = email;
+        }
     }
 }
